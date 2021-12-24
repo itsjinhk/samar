4a35ba2e5352f1cb8b961cf59afdda68cc498255
itsjinhk <darkjinh@gmail.com>
2021-12-25 02:28:23 +0700

    add blog

--- app.js
+++ app.js
@@ -2,3 +2,32 @@ $(".counter").countUp({
   time: 2000,
   delay: 10,
 });
+
+var testimonialThumbs = new Swiper(".testimonial-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  centeredSlides: true,
+  freeMode: true,
+  autoplay: true,
+  loop: true,
+  speed: 3000,
+});
+var testimonialContent = new Swiper(".testimonial-comment", {
+  spaceBetween: 10,
+  autoplay: true,
+  loop: true,
+  speed: 3000,
+  thumbs: {
+    swiper: testimonialThumbs,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+});
